e3faf72dacfb65e5d0693667d2146c3172ed01b9
Felix Uriel Lara Castro <138715170+filyb0t@users.noreply.github.com>
2025-03-01 03:36:19 +0000

    cardgenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const card = document.querySelector(".card");
+  card.classList.add(generateRandomSuit());
+  card.innerHTML = generateRandomNumber();
+};
+
+const generateRandomSuit = () => {
+  const suit = ["diamond", "spade", "heart", "club"];
+  const indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+const generateRandomNumber = () => {
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
 };
